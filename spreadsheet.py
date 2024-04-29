39dc768f740a4db3cae7d0bc2621c0858b9f6c1b
Gustavo Batista Felix <90421959+Gustavobtfelix@users.noreply.github.com>
2024-04-29 14:18:28 -0300

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -3,6 +3,7 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
+import google.auth
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -11,18 +12,18 @@ SERVICE_ACCOUNT_FILE = 'token.json'
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "1fmkG8rR-UssV_cu-DVEReN4ZFITkGCdAACvggjEFHv0"
+SAMPLE_SPREADSHEET_ID = "1JYwyZ-PbrlioRkDMh1tgVYg-cu2P9jzIlZhUCNOrzTU"
 SAMPLE_RANGE_NAME = "Página1!A1:E"
 
 
-
-
-def main():
+def get_values():
   """Shows basic usage of the Sheets API.
   Prints values from a sample spreadsheet.
   """
   creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+  
+  # creds = google.auth.default()
 
   try:
     service = build("sheets", "v4", credentials=creds)
@@ -43,10 +44,10 @@ def main():
     print("Name, Major:")
     for row in values:
       # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[4]}")
+      print(row)
   except HttpError as err:
     print(err)
 
 
 if __name__ == "__main__":
-  main()
+  get_values()
